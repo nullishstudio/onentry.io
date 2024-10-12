abbdae1ce386693621a6f9f43274a9f2c2fd5dbb
Veri5ied <alvinuchenna14@gmail.com>
2024-10-12 08:43:05 +0100

    WIP: more clean ups

--- src/app/more-links/components/more-links.tsx
+++ src/app/more-links/components/more-links.tsx
@@ -26,8 +26,8 @@ const MoreLinksComponent = () => {
   return (
     <div className="mt-1 grid gap-3">
       <p className="text-sm font-normal font-plus-jakarta text-[#667085] max-w-[320px] leading-5">
-        Connect your profiles and share your social presence. Add as many social
-        links as you like to showcase all your platforms.
+        Add as many links as you need, like portfolios, projects, or anything
+        else that represents you.
       </p>
       <div className="bg-[#EAECF0] h-[1px] w-full my-5" />
 
--- src/components/profilebox.tsx
+++ src/components/profilebox.tsx
@@ -10,6 +10,7 @@ import { FaFacebook } from "react-icons/fa6";
 import { SiFarcaster } from "react-icons/si";
 import Link from "next/link";
 import { Link2 } from "iconsax-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const ProfileBox = () => {
   const fetchUserProfile = async () => {
@@ -17,7 +18,7 @@ const ProfileBox = () => {
     return res.data;
   };
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["basicprofile"],
     queryFn: () => fetchUserProfile(),
     retry: false,
@@ -52,21 +53,37 @@ const ProfileBox = () => {
   return (
     <div className="p-6 my-0 mx-auto rounded-2xl">
       <div className="min-h-[300px] bg-white p-[56px] rounded-2xl grid gap-5">
-        <Image
-          src={data?.avatarUrl ? data?.avatarUrl : SampleUser}
-          alt="user_avatar"
-          className="rounded-[100px] w-32 h-32"
-          width={128}
-          height={128}
-        />
+        {isLoading ? (
+          <Skeleton className="rounded-[100px] w-32 h-32" />
+        ) : (
+          <Image
+            src={data?.avatarUrl ? data?.avatarUrl : SampleUser}
+            alt="user_avatar"
+            className="rounded-[100px] w-32 h-32"
+            width={128}
+            height={128}
+          />
+        )}
         <div className="grid gap-1">
-          <h1 className="text-2xl font-bricolage-grotesque font-bold">
-            {data?.fullname}
-          </h1>
-          <span className="text-[13px] text-[#667085]">@{data?.username}</span>
+          {isLoading ? (
+            <>
+              <Skeleton className="h-5 w-[136px] rounded-md" />
+              <Skeleton className="h-3 w-[136px] rounded-md" />
+            </>
+          ) : (
+            <>
+              <h1 className="text-2xl font-bricolage-grotesque font-bold">
+                {data?.fullname}
+              </h1>
+              <span className="text-[13px] text-[#667085] font-plus-jakarta">
+                @{data?.username}
+              </span>
+            </>
+          )}
         </div>
         <p className="text-sm font-plus-jakarta font-normal text-[#667085]">
-          {data?.bio}
+          {isLoading && <Skeleton className="h-28 w-full" />}
+          {!isLoading && <>{data?.bio}</>}
         </p>
         <div className="flex items-center gap-[10px]">
           {data?.social?.length > 0 &&
@@ -80,36 +97,46 @@ const ProfileBox = () => {
         </div>
         <div className="bg-[#EAECF0] h-[1px] w-full my-3" />
         <div className="grid grid-cols-3 gap-3">
-          {data?.extraLinks?.length > 0 &&
-            data?.extraLinks.map(
-              (
-                {
-                  url,
-                  title,
-                  description,
-                }: { url: string; title: string; description: string },
-                idx: number
-              ) => (
-                <div
-                  className="bg-[#F9FAFB] py-2 px-3 min-h-[51px] rounded-xl grid gap-[10px]"
-                  key={idx}
-                >
-                  <button
-                    key={idx}
-                    className="text-sm font-plus-jakarta font-normal text-[#1D2939] flex items-center gap-2"
-                  >
-                    <Link href={url}>
-                      <Link2 size="18" cursor="pointer" color="#7880E9" />
-                    </Link>
-                    {title}
-                  </button>
-                  <p className="text-[13px] text-[#667085] font-plus-jakarta font-medium">{`${description?.slice(
-                    0,
-                    40
-                  )}...`}</p>
-                </div>
-              )
-            )}
+          {isLoading && (
+            <>
+              <Skeleton className="py-2 px-3 min-h-[51px] rounded-xl grid gap-[10px]" />
+              <Skeleton className="py-2 px-3 min-h-[51px] rounded-xl grid gap-[10px]" />
+            </>
+          )}
+          {!isLoading && (
+            <>
+              {data?.extraLinks?.length > 0 &&
+                data?.extraLinks.map(
+                  (
+                    {
+                      url,
+                      title,
+                      description,
+                    }: { url: string; title: string; description: string },
+                    idx: number
+                  ) => (
+                    <div
+                      className="bg-[#F9FAFB] py-2 px-3 min-h-[51px] rounded-xl grid gap-[10px]"
+                      key={idx}
+                    >
+                      <button
+                        key={idx}
+                        className="text-sm font-plus-jakarta font-normal text-[#1D2939] flex items-center gap-2"
+                      >
+                        <Link href={url}>
+                          <Link2 size="18" cursor="pointer" color="#7880E9" />
+                        </Link>
+                        {title}
+                      </button>
+                      <p className="text-[13px] text-[#667085] font-plus-jakarta font-normal">{`${description?.slice(
+                        0,
+                        40
+                      )}...`}</p>
+                    </div>
+                  )
+                )}
+            </>
+          )}
         </div>
       </div>
     </div>
