a6dcb7ee6a8ae1192152dbc1fc2551959278b1ee
Veri5ied <alvinuchenna14@gmail.com>
2024-10-19 09:34:31 +0100

    WIP: manual profile

--- src/app/social-links/components/social-links.tsx
+++ src/app/social-links/components/social-links.tsx
@@ -5,6 +5,25 @@ import { SiFarcaster } from "react-icons/si";
 import { InfoCircle } from "iconsax-react";
 import { Github, Link } from "lucide-react";
 import { AiOutlineDiscord } from "react-icons/ai";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import TextInput from "@/components/input";
+import Spinner from "@/components/spinner";
+import { axiosInstance } from "@/service/api.service";
+import { apiRoutes } from "@/service/api.route";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { dispatchtoast } from "@/components/toast";
+import { toast } from "sonner";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
 
 interface SocialProps {
   data: {
@@ -18,11 +37,16 @@ interface SocialProps {
 }
 
 const SocialLinksComponent = ({ data }: SocialProps) => {
+  const [social, setSocial] = useState<string>("");
+  const [value, setValue] = useState<string>("");
+  const [platform, setPlatform] = useState<string>("");
+  const queryClient = useQueryClient();
   const socialLinks = [
     {
       name: "Farcaster",
       icon: <SiFarcaster width={22} size={22} color="#7880E9" />,
       connected: data?.farcaster,
+      url: data?.farcaster,
     },
     {
       name: "Twitter",
@@ -56,6 +80,33 @@ const SocialLinksComponent = ({ data }: SocialProps) => {
     },
   ] as const;
 
+  const handleSocialUpdate = async () => {
+    const label = platform.toLowerCase();
+    const res = await axiosInstance.patch(apiRoutes.USER, {
+      [label]: value,
+    });
+    return res.data;
+  };
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: () => handleSocialUpdate(),
+    mutationKey: ["updatesocials"],
+    onSuccess: () => {
+      dispatchtoast({
+        text: `${platform} social setup successfully`,
+      });
+      queryClient.refetchQueries({
+        queryKey: ["fulluserforsocial"],
+      });
+      queryClient.refetchQueries({
+        queryKey: ["fulluser"],
+      });
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+
   return (
     <div className="mt-1 grid gap-3">
       <p className="text-sm font-normal font-plus-jakarta text-[#667085] max-w-[320px] leading-5">
@@ -67,11 +118,11 @@ const SocialLinksComponent = ({ data }: SocialProps) => {
           <InfoCircle size="14" width={20} variant="Bold" color="green" />
         </div>
         Your social profile is automatically fetched using basename if connected
-        during setup. Manual connection is still base(ing) soon!
+        during setup. Click on a button to setup or update!
       </div>
       <div className="bg-[#EAECF0] h-[1px] w-full my-5" />
       <div className="grid gap-3">
-        {socialLinks.map(({ name, icon, connected }, idx) => (
+        {socialLinks.map(({ name, icon, connected, url }, idx) => (
           <div
             key={idx}
             className="flex w-full items-center justify-between bg-white py-4 px-5 min-h-[51px] rounded-xl"
@@ -83,10 +134,78 @@ const SocialLinksComponent = ({ data }: SocialProps) => {
               {icon}
               {name}
             </button>
-            <div className="border rounded-[6px] text-xs text-[#667085] cursor-pointer py-[6px] px-2 font-plus-jakarta border-[#EAECF0] hover:bg-[linear-gradient(91deg,_#FCE4FF_2.91%,_#FFEBD7_99.03%)] hover:border-[#FCE4FF]">
-              {connected && "Connected"}
-              {!connected && "Not Connected"}
-            </div>
+
+            <Dialog>
+              <DialogTrigger asChild>
+                {connected ? (
+                  <button
+                    onClick={() => {
+                      setSocial(url);
+                      setPlatform(name);
+                    }}
+                    className={`border rounded-[6px] text-xs text-[#667085] cursor-pointer py-[6px] px-2 font-plus-jakarta border-[#EAECF0] bg-[linear-gradient(91deg,_#FCE4FF_2.91%,_#FFEBD7_99.03%)] hover:border-[#FCE4FF]`}
+                  >
+                    Connected
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      setSocial(url);
+                      setPlatform(name);
+                    }}
+                    className="border rounded-[6px] text-xs text-[#667085] cursor-pointer py-[6px] px-2 font-plus-jakarta border-[#EAECF0] hover:bg-[linear-gradient(91deg,_#FCE4FF_2.91%,_#FFEBD7_99.03%)] hover:border-[#FCE4FF]"
+                  >
+                    Not Connected
+                  </button>
+                )}
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                  <DialogTitle className="font-plus-jakarta">
+                    Complete or update profile{" "}
+                  </DialogTitle>
+                  <DialogDescription className="font-plus-jakarta">
+                    Setup your {platform} profile.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center space-x-2">
+                  <div className="grid flex-1 gap-1">
+                    <Label htmlFor="link" className="sr-only">
+                      Link
+                    </Label>
+                    <TextInput
+                      id="link"
+                      placeholder=""
+                      onChange={(e) => setValue(e.target.value)}
+                      defaultValue={url === social ? url : ""}
+                    />
+                    <div className="flex flex-col md:flex-row gap-2 items-center mt-4">
+                      {!isPending && (
+                        <button
+                          onClick={() => mutate()}
+                          className="text-white min-w-[120px] w-full min-h-14 py-[18px] px-5 rounded-xl bg-[#7880E9] hover:bg-[#7880E9] font-plus-jakarta text-sm font-medium"
+                        >
+                          Save
+                        </button>
+                      )}
+                      {isPending && (
+                        <Button
+                          className="w-full bg-white hover:bg-white border-none shadow-none"
+                          type="button"
+                        >
+                          <Spinner />
+                        </Button>
+                      )}
+                      <DialogClose asChild>
+                        <button className="border border-[#D9E2E7] rounded-xl text-[#212121] bg-white text-sm font-plus-jakarta font-medium w-full min-h-14 hover:bg-white">
+                          Cancel
+                        </button>
+                      </DialogClose>
+                    </div>
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
           </div>
         ))}
       </div>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,11 +3,17 @@ import { Button, ButtonProps } from "./ui/button";
 interface AppButtonProps extends ButtonProps {
   text: string;
   className?: string;
+  onClick?: () => void;
 }
 
-export default function AppButton({ text, className }: AppButtonProps) {
+export default function AppButton({
+  text,
+  className,
+  onClick,
+}: AppButtonProps) {
   return (
     <Button
+      onClick={onClick}
       className={`${
         className && className
       } min-w-[120px] w-full min-h-14 py-[18px] px-5 rounded-xl bg-[#7880E9] hover:bg-[#7880E9] font-plus-jakarta text-base font-bold`}
